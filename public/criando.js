9515401de397a1068c5d3306e35947f9aed9af0f
Rodrigo <rodrigovieira18@gmail.com>
2015-08-13 15:46:20 -0300

    Entidades funcionando.

--- public/criando.js
+++ public/criando.js
@@ -1,31 +1,129 @@
 $(function() {
   var socket = io();
+  var buzzs = [];
+  var typeAndInputs = {
+    'text': ['.local','.text'],
+    'video': ['.local','.text','.video'],
+    'quote': ['.local','.photo','.text'],
+    'photo': ['.local','.photo','.text'],
+    'gallery': ['.local','.photos']
+  };
 
-  function sendMessage () {
-    var local     = $('#burb .local').val();
+
+  function getYoutubeId(url){
+    var regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|\&v=)([^#\&\?]*).*/;
+    var match = url.match(regExp);
+
+    if (match && match[2].length == 11) {
+      return match[2];
+    }else{
+      alert("Url incorrecta");
+    }
+  }
+
+  function sendMessage (buzz) {
+    socket.emit('burburinho', buzz);
+  }
+
+  function addBuzz(){
+    var local     = $('input.local').val();
     var timestamp = moment().format('YYYY-MM-DD HH:mm:ss');
-    var content   = $('#burb .text').val();
-
-    var message = {
+    var content   = $('textarea.text').val();
+    var video      = $('input.video').val();
+    var photo      = $('input.photo').val();
+    var type = $('#type').val();
+    var id = (new Date()).getTime();
+    var buzz = {
       local: local,
       timestamp: timestamp,
-      content: content,
-      type: 'text'
+      type: type
     };
 
-    socket.emit('burburinho',message);
+    if( type === 'video' ){
+      buzz['url'] = '//www.youtube.com/embed/'+getYoutubeId(video);
+    }else if( type === 'photo' ||  type === 'quote'){
+      buzz['url'] = photo;
+    }
+
+    if(content !== ''){
+      buzz['content'] = content;
+    }
+
+    buzzs.push(buzz);
+    updateBuzzList(buzz);
+  }
+
+  function getLabelOfType(type){
+    var result = 'Texto';
+
+    if( type === 'video') result = 'Video';
+    if( type === 'quote') result = 'Aspas';
+    if( type === 'photo') result = 'Foto comentada';
+    if( type === 'gallery') result = 'Galleria de imagens';
+
+    return result;
+  }
+
+  function updateBuzzList(buzz){
+    var list = $('aside .list-buzz');
+    var div = $('<div/>', { class: 'buzz', id:buzz.id, 'data-buzz': JSON.stringify(buzz) });
+
+    div.append(
+        $('<h2/>',{ text: getLabelOfType(buzz.type) }),
+        $('<time>'+buzz.timestamp +'</time>'),
+        $('<p>'+buzz.content+'</p>'),
+        $('<span>'+buzz.local+'</span>'),
+        $('<button/>',{
+            text: 'Enviar',
+            click: function(){
+              sendMessage(buzz);
+            }
+        })
+    );
+
+    list.append(div);
   }
 
   $('#type').on('change', function(e){
     var type = $(this).val();
 
+    if(type !== ''){
+      var inputs = typeAndInputs[type];
+      $('.element').hide();
+      var previewImage = $('img.preview').data('src');
+      $('img.preview').attr('src',previewImage);
 
-    $('.type').hide();
-    $('#'+type).show();
+      inputs.forEach(function(e){
+        $(e).show();
+      });
+
+      $('.type').show();
+    }
   });
 
   $('.go').click(function(){
-    sendMessage();
+    addBuzz();
+  });
+
+  $("div.element.photo").uploadFile({
+    url:"//file-service.herokuapp.com/upload",
+    multiple:false,
+    dragDrop:true,
+    acceptFiles:"image/*",
+    fileName:'myfile',
+    showPreview:false,
+    showStatusAfterSuccess: false,
+    onSubmit:function(files)
+    {
+      $('div.element.photo').hide();
+    },
+    onSuccess:function(files,data,xhr,pd)
+    {
+      var url = data['link'];
+
+      $('img.preview').attr('src', url);
+      $('input.element.photo').val(url);
+    },
   });
 
   $('.type').hide();
